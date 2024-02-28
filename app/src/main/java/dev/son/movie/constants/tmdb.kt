b9af06b1d06eb428e53de69ec768eefa720f0d9b
Congson <suytdeptrai2402@gmail.com>
2024-02-28 17:23:17 +0700

    change link image

--- app/src/main/java/dev/son/movie/constants/tmdb.kt
+++ app/src/main/java/dev/son/movie/constants/tmdb.kt
@@ -2,7 +2,7 @@ package com.netflixclone.constants
 
 import dev.son.movie.network.models.countries.Countries
 
-const val BASE_IMG = "https://img.ophim9.cc/uploads/movies/"
+const val BASE_IMG = "https://img.ophim12.cc/uploads/movies/"
 const val BASE_IMG_HEADER = "https://img.ophim9.cc"
 const val COUNTRIES = "Countries"
 const val CATEGORIES = "Categories"
